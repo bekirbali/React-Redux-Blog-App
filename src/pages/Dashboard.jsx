9ef8f3351de656519e98b4e20037d6308d69994d
bekirbali <bfbali43@gmail.com>
2023-04-22 18:09:07 +0300

    blogCard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
+import { Button, Grid, Typography } from "@mui/material";
+import BlogCard from "../components/BlogCard";
+import { useSelector } from "react-redux";
 
 const Dashboard = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const { blogs } = useSelector((state) => state.blog);
+
   return (
     <>
       <Navbar />
-      <div className="mt-[5rem]">Dashboard</div>
+      <Grid container sx={(flex, { gap: 2 })}>
+        {blogs?.map((blog) => {
+          return (
+            <Grid item key={blog.id}>
+              <BlogCard blog={blog} />
+            </Grid>
+          );
+        })}
+      </Grid>
     </>
   );
 };
