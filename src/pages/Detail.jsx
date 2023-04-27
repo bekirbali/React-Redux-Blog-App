c9a771511e2beb8b739d5e756e4f2d682c22dc47
bekirbali <bfbali43@gmail.com>
2023-04-27 16:33:56 +0300

    blogCall

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,24 +1,29 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { useLocation } from "react-router-dom";
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 
 const Detail = () => {
   const { state: blog } = useLocation();
   console.log(blog);
   return (
-    <>
+    <div className="flex flex-col w-[50%] m-auto p-3">
       <Navbar />
       <Box
         sx={{
           marginTop: "5rem",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
         }}
       >
-        <img src={blog?.image} alt="" />
-        <div>
+        <div className="text-left mt-3 w-[350px] mx-auto shadow-lg p-2">
+          <img
+            style={{ height: "350px", width: "350px", margin: "0 auto" }}
+            src={blog?.image}
+            alt=""
+          />
           <Typography>{blog?.author}</Typography>
           <Typography>
             {new Date(blog?.publish_date).toLocaleDateString("en-us", {
@@ -32,9 +37,45 @@ const Detail = () => {
               day: "numeric",
             })}
           </Typography>
+          <Typography
+            sx={{
+              borderTop: "1px solid black",
+              paddingTop: "1rem",
+              textDecoration: "underline",
+            }}
+          >
+            {blog?.title}
+          </Typography>
+          <Typography>{blog?.content}</Typography>
         </div>
+        <Grid
+          container
+          sx={{
+            justifyContent: "center",
+            marginTop: "1rem",
+            gap: "1rem",
+          }}
+        >
+          {!blog?.likes ? (
+            <Typography>
+              <FavoriteBorderOutlinedIcon /> {blog?.likes}
+            </Typography>
+          ) : (
+            <Typography>
+              <FavoriteOutlinedIcon /> {blog?.likes}
+            </Typography>
+          )}
+
+          <Typography>
+            <ChatBubbleOutlineOutlinedIcon role="button" />{" "}
+            {blog?.comment_count}
+          </Typography>
+          <Typography>
+            <VisibilityOutlinedIcon /> {blog?.post_views}
+          </Typography>
+        </Grid>
       </Box>
-    </>
+    </div>
   );
 };
 
